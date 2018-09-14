5a9bdc3d0befd9f3dfa1ba8274762a39ff15dd46
AlbertoCanoGarcia <36516063+AlbertoCanoGarcia@users.noreply.github.com>
2018-09-14 14:38:41 +0200

    comit 1

---
+++ app/src/main/java/noctis/canox/proyectonoctis/Ventanas/Saldo.java
@@ -0,0 +1,26 @@
+package noctis.canox.proyectonoctis.Ventanas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+import noctis.canox.proyectonoctis.Clases.BaseDatos;
+import noctis.canox.proyectonoctis.R;
+
+public class Saldo extends AppCompatActivity {
+    private TextView txtSaldo;
+    private Double saldo;
+    private BaseDatos bd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saldo);
+        DecimalFormat df = new DecimalFormat("0.00");
+        bd= new BaseDatos(this);
+        txtSaldo=findViewById(R.id.saldo);
+        saldo=bd.cargarIngresosTotal()-bd.cargarGastoTotal();
+        txtSaldo.setText(df.format(saldo));
+    }
+}
